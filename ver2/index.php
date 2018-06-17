a07326332f8ce1404fb9ee3a5207be8d1d60f187
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-06-17 21:50:33 +0800

    linked files to new head file

--- ver2/index.php
+++ ver2/index.php
@@ -1,28 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Find doctors easily and book an appointment online">
-	<meta name="author" content="ict-capstone">
-	<title>FINDING DOCTORS - Find doctors easily and book an appointment online</title>
-
-	<!-- Favicons-->
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-
-	<!-- BASE CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link href="css/menu.css" rel="stylesheet">
-	<link href="css/vendors.css" rel="stylesheet">
-	<link href="css/icon_fonts/css/all_icons_min.css" rel="stylesheet">
-    
-	<!-- CUSTOM CSS -->
-	<link href="css/custom.css" rel="stylesheet">
-	
-</head>
+<?php
+	require('head.php');
+?>
 
 <body>
 	
