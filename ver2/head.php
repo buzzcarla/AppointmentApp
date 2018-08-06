ad64b2712d1143647c723113743e8ae7dc8e206e
Leonille Christie Lavador <leonillechristie@gmail.com>
2018-08-07 00:02:59 +0800

    added styling and new dependencies

--- ver2/head.php
+++ ver2/head.php
@@ -12,6 +12,10 @@
 	<link href="css/menu.css" rel="stylesheet">
 	<link href="css/vendors.css" rel="stylesheet">
 	<link href="css/icon_fonts/css/all_icons_min.css" rel="stylesheet">
+	
+	<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     
 	<!-- CUSTOM CSS -->
 	<link href="css/custom.css" rel="stylesheet">
