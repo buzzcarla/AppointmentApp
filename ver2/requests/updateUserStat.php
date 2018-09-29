1aa90491d82e349a7eca30a57cc8e39da691b0b0
leonillechristie <leonillechristie@gmail.com>
2018-09-29 11:56:33 +0800

    merge with lee branch

---
+++ ver2/requests/updateUserStat.php
@@ -0,0 +1,20 @@
+<?php 
+require_once('../connect.php');
+if(isset($_POST['stat'])){
+    list($stat, $id) = explode("_", $_POST['stat']);
+    echo " ".$stat;
+    $query = "UPDATE `user` 
+                SET `user_status`=".$stat." 
+                WHERE user.user_id =".$id;
+    $res = mysqli_query($mysql,$query);
+    if($res){
+        echo json_encode('1');
+    }else{
+        echo json_encode('0');
+    }
+} else{
+    echo json_encode('2');
+}
+
+
+?>
