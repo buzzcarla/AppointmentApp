73363efd5fc2cb8438e94a560e9f479375474aae
leonillechristie <leonillechristie@gmail.com>
2018-10-01 01:33:42 +0800

    cleaned user pages

--- ver2/index.php
+++ ver2/index.php
@@ -8,50 +8,10 @@
 	}
 ?>
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="Find doctors easily and book an appointment online">
-	<meta name="author" content="ict-capstone">
-	<title>FINDING DOCTORS - Find doctors easily and book an appointment online</title>
 
-	<!-- BASE CSS -->
-	<link href="css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/style.css" rel="stylesheet">
-	<link href="css/menu.css" rel="stylesheet">
-	<link href="css/vendors.css" rel="stylesheet">
-
-	<!-- Favicons-->
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-	<link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
-	<link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
-   
-    <!-- SPECIFIC CSS -->
-    <link href="css/date_picker.css" rel="stylesheet">
-    
-	<!-- YOUR CUSTOM CSS -->
-	<link href="css/custom.css" rel="stylesheet">
-
-
-<!-- Bootstrap CSS -->
-
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
-<!-- Javascripts -->
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
-	<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <script src="js/jquery.bootstrap.year.calendar.min.js"></script>
-	<!-- CUSTOM CSS -->
-	<link href="css/custom.css" rel="stylesheet">
-	<link href="css/jquery.bootstrap.year.calendar.min.css" rel="stylesheet">
+<?php
+	require('head.php');
+?>
 
 </head>
 <body>
@@ -130,12 +90,9 @@
 					</div>
 				</div>
 			</div>
-			<!-- /row -->
-			<p class="text-center" style="text-align: center;"><a href="listall.php" class="btn_1 medium">Find Doctor</a></p>
+			<p class="text-center" style="text-align: center;"><a href="list-2.php" class="btn_1 medium">Find Doctor</a></p>
 
 		</div>
-		<!-- /container -->
-
 		<div class="bg_color_1">
 			<div class="container margin_120_95">
 				<div class="main_title">
@@ -186,10 +143,7 @@
 				</div>
 				<!-- /carousel -->
 			</div>
-			<!-- /container -->
 		</div>
-		<!-- /white_bg -->
-
 		<div class="container margin_120_95" style="padding-top: 50px!important;">
 			<div class="main_title">
 				<h2>Find your doctor or clinic</h2>
@@ -235,17 +189,13 @@
 					</div>
 				</div>
 			</div>
-			<!-- /row -->
 		</div>
-		<!-- /container -->
 	</main>
-	<!-- /main content -->
 	
 	<?php 
 		require('footer.php');
 	?>
 	</div>
-	<!-- page -->
 
 	<div id="toTop">
 		<img class="topArrow" src="./img/up_arrow.png">
--- ver2/list-2.php
+++ ver2/list-2.php
@@ -19,15 +19,12 @@
 	<div id="preloader" class="Fixed">
 		<div data-loader="circle-side"></div>
 	</div>
-	<!-- /Preload-->
 	
 	<div id="page">		
-		<?php
-			require('header.php');
-		?>
+	<?php
+		require('header.php');
+	?>
 
-	<!-- /header -->
-	
 	<main class="theia-exception">
 		<div id="results">
 			<div class="container">
@@ -43,11 +40,8 @@
 						</div>
 					</div>
 				</div>
-				<!-- /row -->
 			</div>
-			<!-- /container -->
 		</div>
-		<!-- /results -->
 
 		<div class="filters_listing" style="margin-bottom: 10px;">
 			<div class="container">
@@ -66,9 +60,7 @@
 					
 				</ul>
 			</div>
-			<!-- /container -->
 		</div>
-		<!-- /filters -->
 		</form>
 
 		<?php 
@@ -134,19 +126,6 @@
 				$res = mysqli_query($mysql,$query);
 				if($res){
 					while($row = mysqli_fetch_array($res)) {
-						// echo '<div class="strip_list">
-						// <a href="#0" class="wish_bt"></a>
-						// <figure>
-						// 	<a href="detail-page-2.php"><img src="img/doctor_listing_1.jpg" alt=""></a>
-						// </figure>';
-						// echo '<small>'.$row[13].'</small>';
-						// echo '<h3>Dr.'.$row[3].'</h3>';
-						// echo '<ul>
-						// 		<li><a href="#0" onclick="onHtmlClick("Doctors", 2)" class="btn_listing">View on Map</a></li>
-						// 		<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank">Directions</a></li>
-						// 		<li><a href="detail-page-2.php">Book now</a></li>
-						// 	</ul> </div>';
-
 						echo '<div class="container margin_60_35" style="padding-top: 0px;">
 							<div class="row">
 								<div class="col-lg-7">
@@ -182,19 +161,6 @@
 				$res2 = mysqli_query($mysql,$query2);
 				if($res2){
 					while($row = mysqli_fetch_array($res2)) {
-						// echo '<div class="strip_list">
-						// <a href="#0" class="wish_bt"></a>
-						// <figure>
-						// 	<a href="detail-page-2.php"><img src="img/doctor_listing_1.jpg" alt=""></a>
-						// </figure>';
-						// echo '<small>'.$row1[1].'</small>';
-						// echo '<h3>'.$row1[4].'</h3>';
-						// echo '<ul>
-						// 		<li><a href="#0" onclick="onHtmlClick("Doctors", 2)" class="btn_listing">View on Map</a></li>
-						// 		<li><a href="https://www.google.com/maps/dir//Assistance+%E2%80%93+H%C3%B4pitaux+De+Paris,+3+Avenue+Victoria,+75004+Paris,+Francia/@48.8606548,2.3348734,14z/data=!4m15!1m6!3m5!1s0x0:0xa6a9af76b1e2d899!2sAssistance+%E2%80%93+H%C3%B4pitaux+De+Paris!8m2!3d48.8568376!4d2.3504305!4m7!1m0!1m5!1m1!1s0x47e67031f8c20147:0xa6a9af76b1e2d899!2m2!1d2.3504327!2d48.8568361" target="_blank">Directions</a></li>
-						// 		<li><a href="detail-page-2.php">Book now</a></li>
-						// 	</ul> </div>';
-
 						echo '<div class="container margin_60_35" style="padding-top: 0px;">
 							<div class="row">
 								<div class="col-lg-7">
@@ -220,7 +186,6 @@
 					}
 				}
 			}else {
-				// echo '<div class="row" ><h1>FOUND NONE</h1></div>';
 				echo 
 					'<div class="container margin_60_35" style="padding-top: 0px;">
 						<div class="row">
@@ -241,21 +206,15 @@
 				}
 			?>
 		</div>
-		<!-- /container -->
 	</main>
-	<!-- /main -->
-	
 	
 	<?php 
 		require('footer.php');
 	?>
 	</div>
-	<!--/footer-->
 	</div>
-	<!-- page -->
 
 	<div id="toTop"></div>
-	<!-- Back to top button -->
 
 	<!-- COMMON SCRIPTS -->
 	<script src="js/jquery-2.2.4.min.js"></script>
@@ -266,7 +225,5 @@
 	<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyCoz993id_Qq7AcfaoVzviC6swgvQRixuQ"></script>
     <script src="js/map_listing.js"></script>
     <script src="js/infobox.js"></script>
-
-
 </body>
 </html>
