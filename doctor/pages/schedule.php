6149fef6c8248e6387fa6a4357cafb1da1360e3b
leonillechristie <leonillechristie@gmail.com>
2018-10-01 01:15:55 +0800

    updated schedule page

--- doctor/pages/schedule.php
+++ doctor/pages/schedule.php
@@ -60,21 +60,25 @@
 				<div class="top-info"><span>Monday</span></div>
 
 				<ul>
-					<li class="single-event" data-start="09:30" data-end="10:30" data-content="HELLO THIS IS THE CONTENTS" data-event="event-1">
+					<!-- DATA CONTENT IS LINKED WITH EVENT MODAL AT THE BOTTOM OF PAGE -->
+					<li class="single-event" data-start="09:30" data-end="10:30" data-content="event-abs-circuit" data-event="event-1">
 						<a href="#0">
-							<em class="event-name">Abs Circuit</em>
+							<em class="event-name">John Doe</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="11:00" data-end="12:30" data-content="event-rowing-workout" data-event="event-2">
 						<a href="#0">
-							<em class="event-name">Rowing Workout</em>
+							<em class="event-name">Jane Doe</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="14:00" data-end="15:15"  data-content="event-yoga-1" data-event="event-3">
 						<a href="#0">
-							<em class="event-name">Yoga Level 1</em>
+							<em class="event-name">Some Name</em>
+							<p>Follow Up Checkup</p>
 						</a>
 					</li>
 				</ul>
@@ -86,25 +90,29 @@
 				<ul>
 					<li class="single-event" data-start="10:00" data-end="11:00"  data-content="event-rowing-workout" data-event="event-2">
 						<a href="#0">
-							<em class="event-name">Rowing Workout</em>
+							<em class="event-name">Joshua Lee</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="11:30" data-end="13:00"  data-content="event-restorative-yoga" data-event="event-4">
 						<a href="#0">
-							<em class="event-name">Restorative Yoga</em>
+							<em class="event-name">Leonille Christie</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="13:30" data-end="15:00" data-content="event-abs-circuit" data-event="event-1">
 						<a href="#0">
-							<em class="event-name">Abs Circuit</em>
+							<em class="event-name">John Doe</em>
+							<p>Prescription</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="15:45" data-end="16:45"  data-content="event-yoga-1" data-event="event-3">
 						<a href="#0">
-							<em class="event-name">Yoga Level 1</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 				</ul>
@@ -116,25 +124,29 @@
 				<ul>
 					<li class="single-event" data-start="09:00" data-end="10:15" data-content="event-restorative-yoga" data-event="event-4">
 						<a href="#0">
-							<em class="event-name">Restorative Yoga</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="10:45" data-end="11:45" data-content="event-yoga-1" data-event="event-3">
 						<a href="#0">
-							<em class="event-name">Yoga Level 1</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="12:00" data-end="13:45"  data-content="event-rowing-workout" data-event="event-2">
 						<a href="#0">
-							<em class="event-name">Rowing Workout</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="13:45" data-end="15:00" data-content="event-yoga-1" data-event="event-3">
 						<a href="#0">
-							<em class="event-name">Yoga Level 1</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 				</ul>
@@ -146,25 +158,29 @@
 				<ul>
 					<li class="single-event" data-start="09:30" data-end="10:30" data-content="event-abs-circuit" data-event="event-1">
 						<a href="#0">
-							<em class="event-name">Abs Circuit</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="12:00" data-end="13:45" data-content="event-restorative-yoga" data-event="event-4">
 						<a href="#0">
-							<em class="event-name">Restorative Yoga</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="15:30" data-end="16:30" data-content="event-abs-circuit" data-event="event-1">
 						<a href="#0">
-							<em class="event-name">Abs Circuit</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="17:00" data-end="18:30"  data-content="event-rowing-workout" data-event="event-2">
 						<a href="#0">
-							<em class="event-name">Rowing Workout</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 				</ul>
@@ -176,19 +192,22 @@
 				<ul>
 					<li class="single-event" data-start="10:00" data-end="11:00"  data-content="event-rowing-workout" data-event="event-2">
 						<a href="#0">
-							<em class="event-name">Rowing Workout</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="12:30" data-end="14:00" data-content="event-abs-circuit" data-event="event-1">
 						<a href="#0">
-							<em class="event-name">Abs Circuit</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 
 					<li class="single-event" data-start="15:45" data-end="16:45"  data-content="event-yoga-1" data-event="event-3">
 						<a href="#0">
-							<em class="event-name">Yoga Level 1</em>
+							<em class="event-name">Bla Bla</em>
+							<p>Consultation</p>
 						</a>
 					</li>
 				</ul>
