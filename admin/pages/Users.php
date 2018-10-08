7093edf6f77a0686d13879ced8c05c09c571fed8
Leonille Christie <leonillechristie@gmail.com>
2018-10-08 11:30:07 +0800

    cleaned admin pages

--- admin/pages/Users.php
+++ admin/pages/Users.php
@@ -1,150 +1,144 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-<?php
-    require('head.php');
-?>
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <?php 
-            require ('nav.php');
+    <!-- global head file -->
+    <head>
+        <?php
+            require('head.php');
         ?>
-        <div id="page-wrapper">
-            <img src="https://www.freewebheaders.com/wordpress/wp-content/gallery/medical/medical-doctor-stethoscope-and-office-sign-web-header.jpg" style=" width: 1235px; height: auto; position: absolute;">
-            <div class="row">
-                <div class="col-lg-12" style="margin-top: 80px">
-                    <h1 class="page-header" style="font-size: -webkit-xxx-large;padding-left: 200px;">Users Management</h1>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Users List
-                        </div>
-                        <div class="panel-body">
-                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
-                                <thead>
-                                    <tr style="background-color:#003a6f!important; color:white;">
-                                        <th>First Name</th>
-                                        <th>Last Name</th>
-                                        <th>Clinic Name</th>
-                                        <th>Address</th>
-                                        <th>Status</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr class="odd gradeX">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="even gradeC">
-                                        <<td>Hello There</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="odd gradeA">
-                                        <td>John Smith</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="odd gradeA">
-                                        <td>John Smith</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="odd gradeA">
-                                        <td>John Smith</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="even gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="odd gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="even gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
-                                    <tr class="gradeA">
-                                        <td>John Doe</td>
-                                        <td>12349823</td>
-                                        <td>Mactan Doctors</td>
-                                        <td class="center">Mactan, Lapu-Lapu, Cebu</td>
-                                        <td class="center">Active</td>
-                                    </tr>
+    </head>
 
-                                </tbody>
-                            </table>
+    <body>
+        <div id="wrapper">
+            <!-- Navigation -->
+            <?php 
+                require ('nav.php');
+            ?>
+            <div id="page-wrapper">
+                <img src="https://www.freewebheaders.com/wordpress/wp-content/gallery/medical/medical-doctor-stethoscope-and-office-sign-web-header.jpg" style=" width: 1235px; height: auto; position: absolute;">
+                <div class="row">
+                    <div class="col-lg-12" style="margin-top: 80px">
+                        <h1 class="page-header" style="font-size: -webkit-xxx-large;padding-left: 200px;">Users Management</h1>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Users List
+                            </div>
+                            <div class="panel-body">
+                                <!-- User table -->
+                                <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr style="background-color:#003a6f!important; color:white;">
+                                            <th>First Name</th>
+                                            <th>Last Name</th>
+                                            <th>Clinic Name</th>
+                                            <th>Address</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="odd gradeX">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="even gradeC">
+                                            <<td>Hello There</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>John Smith</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>John Smith</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>John Smith</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="odd gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="even gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                        <tr class="gradeA">
+                                            <td>John Doe</td>
+                                            <td>12349823</td>
+                                            <td>Mactan Doctors</td>
+                                            <td class="center">Mactan, Lapu-Lapu, Cebu</td>
+                                            <td class="center">Active</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-    <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
-    <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
-    <script src="../dist/js/sb-admin-2.js"></script>
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+        <script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+        <script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+        <script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+        <script src="../dist/js/sb-admin-2.js"></script>
 
-    <script>
-    $(document).ready(function() {
-        $('#dataTables-example').DataTable({
-            responsive: true
+        <script>
+        $(document).ready(function() {
+            $('#dataTables-example').DataTable({
+                responsive: true
+            });
         });
-    });
-    </script>
-
-</body>
-
+        </script>
+    </body>
 </html>
--- admin/pages/profile.php
+++ admin/pages/profile.php
@@ -1,72 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-
-<?php
-    require('head.php');
-?>
-
-</head>
-
-<body>
-
-    <div id="wrapper">
-
-        <!-- Navigation -->
-        <?php 
-            require ('nav.php');
+    <!-- head file -->
+    <head>
+        <?php
+            require('head.php');
         ?>
+    </head>
+    <body>
+        <div id="wrapper">
+            <!-- Navigation -->
+            <?php 
+                require ('nav.php');
+            ?>
 
-        <div id="page-wrapper">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h1 class="page-header">Admin Profile</h1>
+            <div id="page-wrapper">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">Admin Profile</h1>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            Update Admin Information
-                        </div>
-                        <div class="panel-body">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <form role="form">
-                                        <div class="form-group">
-                                            <label>First Name</label>
-                                            <input class="form-control" placeholder="Enter First Name">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Last Name</label>
-                                            <input class="form-control" placeholder="Enter Last Name">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Upload Photo</label>
-                                            <input type="file">
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Radio Buttons</label>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Male
-                                                </label>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                Update Admin Information
+                            </div>
+                            <div class="panel-body">
+                                <div class="row">
+                                    <div class="col-lg-12">
+                                        <form role="form">
+                                            <div class="form-group">
+                                                <label>First Name</label>
+                                                <input class="form-control" placeholder="Enter First Name">
                                             </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Female
-                                                </label>
+                                            <div class="form-group">
+                                                <label>Last Name</label>
+                                                <input class="form-control" placeholder="Enter Last Name">
                                             </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Other
-                                                </label>
+                                            <div class="form-group">
+                                                <label>Upload Photo</label>
+                                                <input type="file">
                                             </div>
-                                        </div>
-                                        <button type="submit" class="btn btn-default">Update</button>
-                                        <button type="reset" class="btn btn-default">Reset</button>
-                                    </form>
+                                            <div class="form-group">
+                                                <label>Radio Buttons</label>
+                                                <div class="radio">
+                                                    <label>
+                                                        <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Male
+                                                    </label>
+                                                </div>
+                                                <div class="radio">
+                                                    <label>
+                                                        <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">Female
+                                                    </label>
+                                                </div>
+                                                <div class="radio">
+                                                    <label>
+                                                        <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">Other
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <button type="submit" class="btn btn-default">Update</button>
+                                            <button type="reset" class="btn btn-default">Reset</button>
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -74,12 +70,11 @@
                 </div>
             </div>
         </div>
-    </div>
-
-    <script src="../vendor/jquery/jquery.min.js"></script>
-    <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../vendor/metisMenu/metisMenu.min.js"></script>
-    <script src="../dist/js/sb-admin-2.js"></script>
-</body>
+        <!-- COMMON JS SCRIPTS -->
+        <script src="../vendor/jquery/jquery.min.js"></script>
+        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../vendor/metisMenu/metisMenu.min.js"></script>
+        <script src="../dist/js/sb-admin-2.js"></script>
+    </body>
 
 </html>
